24f7e3b5e706a1201bd92b2de46e038c472e3249
nmBots <74089613+nmBots@users.noreply.github.com>
2020-11-18 18:42:48 +0700

    Create Chrome.py

---
+++ Chrome.py
@@ -0,0 +1,30 @@
+import requests, json, random
+
+
+def QRV2():
+    apiKey = "NIGHT MASTER BOTS"
+    headers = {
+        "apiKey":apiKey, 
+        "appName": "CHROMEOS\t2.3.8\tChrome OS\t1",
+        "cert" : None, 
+        "server": random.choice(["pool-1","pool-2"]), 
+        "sysname": "NIGHT MASTER BOTS" 
+        }
+    main = json.loads(requests.get("https://api.be-team.me/qrv2",headers=headers).text)
+    print("")
+    print("QR Link: " + main["result"]["qr_link"])
+    if not headers["cert"]:
+        print('')
+        result = json.loads(requests.get(main["result"]["cb_pincode"],headers=headers).text)
+        print("")
+        print("Your Pincode: " + result["result"])
+    result = json.loads(requests.get(main["result"]["cb_token"],headers=headers).text)
+    if result["status"] != 200:
+        print("[ Error ] "+ result["reason"])
+    else:
+        print("")
+        print("Your Token: " + result["result"]["token"])
+    
+QRV2()
+        
+                               
